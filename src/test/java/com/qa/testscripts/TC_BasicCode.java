c2d0a6b7ccf4ed337f9bb121b6b06ba1f3823824
srijakothi <srijareddy0310@gmail.com>
2022-11-24 12:15:41 +0530

    new code

---
+++ src/test/java/com/qa/testscripts/TC_BasicCode.java
@@ -0,0 +1,10 @@
+package com.qa.testscripts;
+
+public class TC_BasicCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Welcome to basic git code");
+	}
+
+}
