8ff3e1a99ba090c0414ec5d87c6445bb48d89248
srijakothi <srijareddy0310@gmail.com>
2022-11-24 11:46:30 +0530

    First Commit

---
+++ src/test/java/com/qa/testscripts/TC_WebDriverManagerDemo.java
@@ -0,0 +1,21 @@
+package com.qa.testscripts;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_WebDriverManagerDemo {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\kothi\\Desktop\\DriverExecutables\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver cdriver = new ChromeDriver();
+		System.setProperty("webdriver.edge.driver","C:\\Users\\kothi\\Desktop\\DriverExecutables\\edgedriver_win64\\msedgedriver.exe");
+		EdgeDriver edriver = new EdgeDriver();
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver cdriver1 = new ChromeDriver();
+		WebDriverManager.edgedriver().setup();
+		EdgeDriver edriver1 = new EdgeDriver();
+		System.out.println("successfully opened");
+
+	}
+}
