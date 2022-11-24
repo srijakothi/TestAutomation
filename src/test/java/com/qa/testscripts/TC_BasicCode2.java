33cddeea23e63c276e3b8088c8604a06e17cbebb
srijakothi <srijareddy0310@gmail.com>
2022-11-24 13:08:16 +0530

    new code added

---
+++ src/test/java/com/qa/testscripts/TC_BasicCode2.java
@@ -0,0 +1,11 @@
+package com.qa.testscripts;
+
+public class TC_BasicCode2 {
+
+	public static void main(String[] args) {
+    System.out.println("Welcome to basic git code");
+    System.out.println("Welcome to basic code");
+	}
+
+
+}
